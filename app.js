3baaee0bf102505806f1850586f5f1c193b6bf97
Ismael <ismaelazd@hotmail.com>
2019-11-18 22:20:01 +0100

    implementations de fonctions

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
-let scores, roundScores, activeayer,dice;
+let scores, roundScore, activeayer,dice;
 
 scores = [0,0];
-roundScores = 0 ;
+roundScore = 0 ;
 activePlayer = 0;
 
 
@@ -10,15 +10,15 @@ activePlayer = 0;
 // permet de rajouter du code html ici mettre en italique 
 //document.querySelector('#current-'+ activePlayer).innerHTML = '<em>' + dice +'</em>' 
 
+// permet de modifier le css ici ne plus afficher l'image
+document.querySelector('.dice').style.display = 'none'; 
+
 document.querySelector('#score-0').textContent = '0';
 document.querySelector('#score-1').textContent = '0';
 document.querySelector('#current-0').textContent = '0';
 document.querySelector('#current-1').textContent = '0';
 
-
-// permet de modifier le css ici ne plus afficher l'image
-document.querySelector('.dice').style.display = 'none'; 
-
+// quand je click sur le bouton roll
 document.querySelector('.btn-roll').addEventListener('click', function(){
 
     //1. Random number
@@ -30,4 +30,31 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     diceDOM.src = 'dice-' + dice + '.png';
 
     //3. Update the round score IF the rolled number was NOT a 1
+    if(dice !== 1){
+        //Add score 
+        roundScore += dice;
+        document.querySelector('#current-'+ activePlayer).textContent = roundScore;
+    }else{
+        //Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; 
+        roundScore = 0; 
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        // remove the active class from the player 1
+        //document.querySelector('.player-0-pannel').classList.remove('active');
+        // ADD the active class to the player 2
+        //document.querySelector('.player-1-pannel').classList.add('active');
+        
+        // permet de rajouter une classe si elle existe pas ou de la supprimer si elle existe 
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        // permet de modifier le css ici ne plus afficher l'image
+        document.querySelector('.dice').style.display = 'none'; 
+
+
+    }
 } )
+
