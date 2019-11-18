2c02e0e4e9a1a1572a27b4679bf9fef4eaac4166
Ismael <ismaelazd@hotmail.com>
2019-11-18 23:14:13 +0100

    Faire en sorte que le jeu s'arrete quand il y'a un gagnant

--- app.js
+++ app.js
@@ -35,26 +35,53 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         roundScore += dice;
         document.querySelector('#current-'+ activePlayer).textContent = roundScore;
     }else{
-        //Next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; 
-        roundScore = 0; 
-
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        // remove the active class from the player 1
-        //document.querySelector('.player-0-pannel').classList.remove('active');
-        // ADD the active class to the player 2
-        //document.querySelector('.player-1-pannel').classList.add('active');
         
-        // permet de rajouter une classe si elle existe pas ou de la supprimer si elle existe 
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        // permet de modifier le css ici ne plus afficher l'image
-        document.querySelector('.dice').style.display = 'none'; 
-
+        nextPlayer();
 
     }
 } )
 
+
+// quand je click sur le bouton hold
+document.querySelector('.btn-roll').addEventListener('click', function(){
+
+    // Add CURRENT score to global score
+    scores[activePlayer] += roundScore;
+
+    // Update UI
+    document.querySelector('#score' + activePlayer).textContent = scores[activePlayer];
+
+    // Check if the player won the game
+    if(scores[activePlayer] >= 100){
+        document.querySelector('#name'+activePlayer).textContent = 'Winner !';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+        document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+
+    }else{
+        nextPlayer();
+    }
+    
+   
+})
+
+function nextPlayer(){
+    //Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; 
+    roundScore = 0; 
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    // remove the active class from the player 1
+    //document.querySelector('.player-0-pannel').classList.remove('active');
+    // ADD the active class to the player 2
+    //document.querySelector('.player-1-pannel').classList.add('active');
+    
+    // permet de rajouter une classe si elle existe pas ou de la supprimer si elle existe 
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    // permet de modifier le css ici ne plus afficher l'image
+    document.querySelector('.dice').style.display = 'none'; 
+}
